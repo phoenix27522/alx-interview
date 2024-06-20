0c9ce2225a3a7f5d1c85f8afe16f69d774a1d340
Nahom kiflu <kiflunahom1994@gmail.com>
2024-06-20 21:02:04 +0300

    fix task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,10 +1,4 @@
-#!/usr/bin/python3
-'''Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet
-    a given amount total.
-'''
 import sys
-
 
 def makeChange(coins, total):
     '''
@@ -14,16 +8,17 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
 
-    if table[total] == sys.maxsize:
-        return -1
-    return table[total]
+    # Create a list to store the minimum number of coins for each value up to total
+    dp = [sys.maxsize] * (total + 1)
+    dp[0] = 0  # Base case: no coins are needed to make 0
+
+    # Iterate through all amounts from 1 to total
+    for i in range(1, total + 1):
+        # Try every coin that doesn't exceed the current amount
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    # If dp[total] is still sys.maxsize, it means it's not possible to make the total with given coins
+    return dp[total] if dp[total] != sys.maxsize else -1
